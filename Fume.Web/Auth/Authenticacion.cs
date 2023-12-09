63df1da3e16cfb0ca87d9937ff41bca34f9c7ce6
usuario <usuario@DESKTOP-0QT03J2>
2023-12-09 09:36:20 -0400

    auto

---
+++ Fume.Web/Auth/Authenticacion.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Fume.Web.Auth
+{
+    public class Authenticacion : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+        }
+    }
+}
--- Fume.Web/Program.cs
+++ Fume.Web/Program.cs
@@ -1,14 +1,18 @@
-using CurrieTechnologies.Razor.SweetAlert2;
 using Fume.Web;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Fume.Web.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
+using Fume.Web.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7181/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7181/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, Authenticacion>();
 await builder.Build().RunAsync();
